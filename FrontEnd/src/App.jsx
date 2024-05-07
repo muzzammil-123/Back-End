dbe04ce3e43a9d7e8b861c6586791b6bcb14b97e
muzzammil-123 <146634572+muzzammil-123@users.noreply.github.com>
2024-05-07 14:44:22 +0500

    d

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,13 +1,17 @@
-import React, { useState } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function App() {
-  const [data, setData] = useState([])
-  axios.get('/api/user').then(res => setData(res.data))
-  console.log(data)
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/user").then((res) => setData(res.data));
+  }); 
+  console.log(data);
+
   return (
     <>
       <h1>Hello World!</h1>
     </>
-  )
+  );
 }
