c5eae99a0e536fb7ae96274f2ef4b1551e2d06d2
professionaltechs <146634572+muzzammil-123@users.noreply.github.com>
2024-05-12 16:27:15 +0530

    mongoose

---
+++ Mongoose/models/user.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        lowercase: true,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+        required : [true, "Password is Required"],
+        maxlength: 16
+    },
+    age: {
+        type: Number,
+        type: Number,
+        min: 18,
+        max: 100,
+        required : [true, "Age is required"],
+    }
+}, {timestamps : true})
+
+
+export default mongoose.model("User", userSchema)
